a835fb337486577c7a7e8b7bfbd374a5755bf518
Nusferaty <margorelik2013@yandex.ru>
2022-12-25 01:21:22 +0300

    front_3

--- www/html/cart_good.php
+++ www/html/cart_good.php
@@ -53,22 +53,29 @@
   </div>
 </header>
 <body>
+  <div class = "colon">
   <div class = "cart">
-  <div class = "part_1">
-  <p class = "text" ><?=  $good['name'] ?></p>
-  <p class = "text" >Описание товара</p>
-        <p class = "text" ><?=  $good['info'] ?></p>
-  </div>
-  <div class = "part_2">
-  <img src="<?=  $good['picture'] ?>" class="img" alt="<?=  $good['name'] ?>">
-        <p class = "text" >Цена: <?=  $good['price'] ?></p>
-  </div>
-      <?php if (!empty ($messages)) {foreach ($messages as $message): ?>
-          <div class = "mes">
-              <p class = "text" ><?=  $message['person_name'] ?></p>
-              <p class = "text" ><?=  $message['message'] ?></p>
-          </div>
-      <?php endforeach; }?>
+    <div class = "part_1">
+      <div class = "part_text">
+      <p class = "text"><?=  $good['name'] ?></p>
+      </div>
+      <p class = "text" >Описание товара:</p>
+      <p class = "text" ><?=  $good['info'] ?></p>
+    </div>
+    <div class = "part_2">
+    <img src="<?=  $good['picture'] ?>" class="img" alt="<?=  $good['name'] ?>">
+    <p class = "text" >Цена: <?=  $good['price'] ?></p>
+    </div>
+    </div>
+    <div class = "otz">
+    <p class = "text" >Отзывы:</p>
+    <?php if (!empty ($messages)) {foreach ($messages as $message): ?>
+      <div class = "mes">
+          <p class = "text" ><?=  $message['person_name'] ?></p>
+          <p class = "text" ><?=  $message['message'] ?></p>
+      </div>
+    <?php endforeach; }?>
+    </div>
   </div>
 </body>
 </html>
